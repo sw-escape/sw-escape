19db50fc2f8ba004438a82dd126c3e3a028d101f
devJS00 <jisuejs@gmail.com>
2023-12-04 08:52:11 +0900

    save

--- lib/cau_common/chinese_character.dart
+++ lib/cau_common/chinese_character.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../sideMenu.dart';
 
-class ChineseCharacter extends StatelessWidget {
+class ChineseCharacter extends StatefulWidget {
   const ChineseCharacter({super.key});
 
+  @override
+  State<ChineseCharacter> createState() => _ChineseCharacterState();
+}
+
+class _ChineseCharacterState extends State<ChineseCharacter> {
+  bool isTOEICLevel5OrHigher = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,18 +38,99 @@ class ChineseCharacter extends StatelessWidget {
       body: Container(
         // color: Colors.blue,
         color: const Color(0xFF72BBFF),
-        child: Column(
+        child: ListView(
           children: [
-            Container(
-              height: 30,
-              // color: Colors.indigo,
+            Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15), // 둥근 모서리
+                    border: Border.all(color: Colors.black), // 까만색 테두리
+                    color: Colors.white, // 하얀색 배경
+                  ),
+                  padding: const EdgeInsets.all(20), // 안쪽 여백 설정
+                  child: const Text(
+                    '한문 수업 1과목 수강\nOR 한자 시험 4급 이상',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15), // 둥근 모서리
+                    border: Border.all(color: Colors.black), // 까만색 테두리
+                    color: Colors.white, // 하얀색 배경
+                  ),
+                  child: CheckboxListTile(
+                    title: const Text(
+                      '한자 시험 4급 이상',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                    value: isTOEICLevel5OrHigher,
+                    onChanged: (newValue) {
+                      setState(() {
+                        isTOEICLevel5OrHigher = newValue!;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Colors.black),
+                    color: Colors.white,
+                  ),
+                  child: const Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              '한자 수업',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: '수강한 과목명 입력',
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black), // 테두리 색상 변경
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+
+                //const ProgressBar(),
+                Container(
+                  height: 60,
+                  // color: Colors.indigo,
+                ),
+                //const CommonMenu(),
+              ],
             ),
-            //const ProgressBar(),
-            Container(
-              height: 60,
-              // color: Colors.indigo,
-            ),
-            //const CommonMenu(),
           ],
         ),
       ),
